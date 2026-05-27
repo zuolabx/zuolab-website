6c9d0585c739c339e1c9165e71b7ffd259d953ba
sahilnyk <contactsahilpnayak@gmail.com>
2026-05-27 23:17:20 +0530

    Add ngrok allowed dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    'ec9e-2405-201-a014-5885-c9d0-f15c-2917-cb4f.ngrok-free.app'
+  ],
 };
 
 export default nextConfig;
